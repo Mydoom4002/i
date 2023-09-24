5882dc64ce8da558c63de0b8cd3e4ba504b3d619
Mydoom4002 <138235487+Mydoom4002@users.noreply.github.com>
2023-09-24 10:02:44 +0530

    Delete manifest/files directory

--- manifest/files/Bruh.bat
+++
@@ -1,6 +0,0 @@
-@echo off                                                         
-echo MMMMMMMMMM  MM    MM  MMMMMMMM    MMMMMMMMMM  MMMMMMMMMM  MMMMMMMMMM  MM    MM  MMMMMMMMM MMMMMMMMM  MMMMMMMM
-echo MM  MM  MM  MM    MM  MM     MM   MM      MM  MM      MM  MM  MM  MM  MM    MM  MM     MM MM     MM      MMMM
-echo MM  MM  MM    MMMM    MM     MM   MM      MM  MM      MM  MM  MM  MM  MMMMMMMM  MM     MM MM     MM  MMMM     
-echo MM  MM  MM     MM     MMMMMMM     MMMMMMMMMM  MMMMMMMMMM  MM  MM  MM        MM  MMMMMMMMM MMMMMMMMM  MMMMMMMM
-pause
