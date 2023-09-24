d91f84bd44e6d9ef0791d3b1073ba6d4e1090e09
Mydoom4002 <138235487+Mydoom4002@users.noreply.github.com>
2023-09-24 10:03:42 +0530

    Create Bruh.bat

---
+++ files/Bruh.bat
@@ -0,0 +1,6 @@
+@echo off                                                         
+echo MMMMMMMMMM  MM    MM  MMMMMMMM    MMMMMMMMMM  MMMMMMMMMM  MMMMMMMMMM  MM    MM  MMMMMMMMM MMMMMMMMM  MMMMMMMM
+echo MM  MM  MM  MM    MM  MM     MM   MM      MM  MM      MM  MM  MM  MM  MM    MM  MM     MM MM     MM      MMMM
+echo MM  MM  MM    MMMM    MM     MM   MM      MM  MM      MM  MM  MM  MM  MMMMMMMM  MM     MM MM     MM  MMMM     
+echo MM  MM  MM     MM     MMMMMMM     MMMMMMMMMM  MMMMMMMMMM  MM  MM  MM        MM  MMMMMMMMM MMMMMMMMM  MMMMMMMM
+pause
